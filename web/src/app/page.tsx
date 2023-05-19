6b0e9b88593c4f5c3f56d98a967fccb431d71609
GuiBarros0 <guisanches.exe@gmail.com>
2023-05-19 12:43:11 -0300

    empty memories

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <main></main>
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="flex flex-col items-start justify-between px-28 py-16">
+      
+      </div>
+
+      {/* Rigth */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w=[360] text-center leading-relaxed">
+            Voçê ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
